77f9811e9c4bb3b27172d57480ba7df8f3c630dc
Leon <cloudjoan@gmail.com>
2022-09-23 02:34:22 +0800

    modify

--- WebApplication3/Controllers/WeatherForecastController.cs
+++ WebApplication3/Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ namespace WebApplication3.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching", "Taichung"
     };
 
         private readonly ILogger<WeatherForecastController> _logger;
